ec0708123148939f6db921ba63d76db236c4ef0a
yanyanshan123456 <yanyanshan@do1.com.cn>
2019-12-12 17:16:34 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
+// import 'reset-css';
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,//在ES6中，键名等于值时，可以简写成一个，也可以写成router:router
--- src/router/index.js
+++ src/router/index.js
@@ -10,14 +10,14 @@ const routes = [
     name: 'home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/list',
+  //   name: 'list',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import( '../views/List.vue')
+  // }
 ]
 
 const router = new VueRouter({
@@ -27,3 +27,5 @@ const router = new VueRouter({
 })
 
 export default router
+
+// 路由就是根据网址的不同，返回不同的内容给用户。
